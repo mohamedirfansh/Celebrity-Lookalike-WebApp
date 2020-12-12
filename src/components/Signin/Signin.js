8687a892549cfb17fc91ca29521dabf882541935
Mohamed Irfan <irfancuber@gmail.com>
2020-12-13 03:13:14 +0800

    Added state to Signin.js and connected signin endpoint to backend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,35 +1,73 @@
 import React from 'react';
 import './Signin.css';
 
-const Signin = ({onRouteChange}) => {
-    return (
-        <article className="br2 ba white mv4 w-100 w-50-m w-25-l mw5 center">
-            <main className="pa4 white signin">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f2 fw6 ph0 mh0">Sign In</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input className="card pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input className="card b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                    </div>
-                    </fieldset>
-                    <div className="">
-                    <input 
-                    onClick={() => onRouteChange('home')}
-                    className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib submit" type="submit" value="Sign in" />
-                    </div>
-                    <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChange('register')} className="register f6 link dim white db">Register</p>
-                    </div>
-                </div>
-            </main>
-        </article>
+class Signin extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+    onEmailChange = (event) => {
+        this.setState({signInEmail: event.target.value})
+    }
 
-    );
+    onPasswordChange = (event) => {
+        this.setState({signInPassword: event.target.value})
+    }
+
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+        .then(response = response.json())
+        .then(data => {
+            if (data === 'success') {
+                this.props.onRouteChange('home');
+            }
+        })
+    }
+    render() {
+        const {onRouteChange} = this.props;
+        return (
+            <article className="br2 ba white mv4 w-100 w-50-m w-25-l mw5 center">
+                <main className="pa4 white signin">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f2 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input className="card pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" name="email-address" id="email-address"
+                            onChange={this.onEmailChange} />
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="card b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="password" name="password" id="password" 
+                            onChange={this.onPasswordChange}/>
+                        </div>
+                        </fieldset>
+                        <div className="">
+                        <input 
+                        onClick={this.onSubmitSignIn}
+                        className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib submit" type="submit" value="Sign in" />
+                        </div>
+                        <div className="lh-copy mt3">
+                            <p onClick={() => onRouteChange('register')} className="register f6 link dim white db">Register</p>
+                        </div>
+                    </div>
+                </main>
+            </article>
+
+        );
+    }
 }
 
 export default Signin;
